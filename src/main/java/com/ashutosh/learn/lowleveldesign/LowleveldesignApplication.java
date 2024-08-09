f9a06548ef721c19f6dfcd79bf9f547ff3b28ab5
Ashutosh Tripathi <ashutoshtripathi@Ashutoshs-MacBook-Air.local>
2024-08-10 01:07:44 +0530

    first commit

---
+++ src/main/java/com/ashutosh/learn/lowleveldesign/LowleveldesignApplication.java
@@ -0,0 +1,13 @@
+package com.ashutosh.learn.lowleveldesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowleveldesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowleveldesignApplication.class, args);
+	}
+
+}
